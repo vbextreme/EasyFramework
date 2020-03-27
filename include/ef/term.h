398bacb1f29c16880d054aa2e29ac83cbba36256
vbextreme <vbextreme@hotmail.com>
2020-03-27 09:34:42 +0100

    include all term header

---
+++ include/ef/term.h
@@ -0,0 +1,10 @@
+#ifndef __EF_TERM_H__
+#define __EF_TERM_H__
+
+#include <ef/type.h>
+#include <ef/terminfo.h>
+#include <ef/termcapstr.h>
+#include <ef/termlink.h>
+#include <ef/termmode.h>
+
+#endif 
