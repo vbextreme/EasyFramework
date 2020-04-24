26a361fbbe1b83e6d0d7570f3f3028e03a03a53d
vbextreme <vbextreme@hotmail.com>
2020-04-24 19:00:24 +0200

    dbg g2d_load

--- ef/graphics/imageFiles.c
+++ ef/graphics/imageFiles.c
@@ -15,6 +15,7 @@ g2dImage_s* g2d_load(char const* path, unsigned width, unsigned height, int rati
 	errno = 0;
    	g2dImage_s* ret = g2d_load_png(path);
 	if( ret ){
+		dbg_info("return png");
 		if( width && height ){
 			g2d_ratio(ratio, ret->w, ret->h, &width, &height); 
 			g2dImage_s* scaled = g2d_resize(ret, width, height);
@@ -31,6 +32,7 @@ g2dImage_s* g2d_load(char const* path, unsigned width, unsigned height, int rati
 	errno = 0;
 	ret = g2d_load_jpeg(path);
 	if( ret ){
+		dbg_info("return jpeg");
 		if( width && height ){
 			g2d_ratio(ratio, ret->w, ret->h, &width, &height); 
 			g2dImage_s* scaled = g2d_resize(ret, width, height);
@@ -47,6 +49,7 @@ g2dImage_s* g2d_load(char const* path, unsigned width, unsigned height, int rati
 	errno = 0;
 	ret = g2d_load_bmp(path);
 	if( ret ){
+		dbg_info("return bmp");
 		if( width && height ){
 			g2d_ratio(ratio, ret->w, ret->h, &width, &height); 
 			g2dImage_s* scaled = g2d_resize(ret, width, height);
@@ -64,6 +67,7 @@ g2dImage_s* g2d_load(char const* path, unsigned width, unsigned height, int rati
 	g2d_ratio(ratio, ret->w, ret->h, &width, &height);
 	ret = g2d_load_svg(path, width, height);
 	if( ret ){
+		dbg_info("return svg");
 		return ret;
 	}
 	if( errno != 666 ){
